a2ce2715994872f2c11807b7956700d29a7520b6
neron2150 <neron2150@gmail.com>
2019-02-12 15:40:29 +0300

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import './App.css';
+import Todos from './Todos';
+import Universities from './Universities';
+class App extends Component {
+
+
+  render() {
+    
+    return (
+      <div>
+        <Universities/>
+      </div>
+    );
+  }
+}
+
+export default App;
