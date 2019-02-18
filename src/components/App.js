5b54d92b05a4f85a4cab4c6d6d558ff7d1197deb
neron2150 <neron2150@gmail.com>
2019-02-18 18:20:10 +0300

    Movable tests in App.js

--- src/components/App.js
+++ src/components/App.js
@@ -2,15 +2,38 @@ import React, { Component } from 'react';
 import './App.css';
 import Todos from './Todos';
 import Universities from './Universities';
+import Movable from './Movable';
+import Movable0 from './Movable0';
+import Movable1 from './Movable1';
+import DropableContainer from './DropableContainer';
+import DragArea from './DragArea';
+import Draggable from './Draggable';
+
 class App extends Component {
 
-
   render() {
-    
+
     return (
-      <div>
-        <Universities/>
-      </div>
+
+
+      <DragArea className='app'>
+        <DropableContainer>
+          <Draggable>
+          <p> ЭЭЭЭЙ</p>
+            <Movable1/>
+          </Draggable>
+
+        </DropableContainer>
+
+        <Movable0/>
+        <Movable/>
+
+      </DragArea>
+
+
+
+
+
     );
   }
 }
