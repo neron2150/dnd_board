52d2d3a0839b4707521575a7301e4918b564348a
neron2150 <neron2150@gmail.com>
2019-02-18 18:17:45 +0300

    Create instances

---
+++ src/components/Draggable.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+class Draggable extends Component{
+  render() {
+    return(
+      <div>
+        {this.props.children}
+      </div>
+    );
+  }
+
+
+
+
+}
+export default Draggable;
